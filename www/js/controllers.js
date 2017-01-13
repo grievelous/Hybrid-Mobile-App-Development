cf23de5edf18d9dfbbbc3ce26b9574151e4755f6
Espen Reksten Berentsen <espenrberentsen@gmail.com>
2017-01-13 09:49:04 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -178,9 +178,7 @@ angular.module('conFusion.controllers', [])
                 $scope.promotion = menuFactory.getPromotion().get({id:0});
 }])
 
-.controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
 
-            $scope.baseURL = baseURL;
             $scope.leaders = corporateFactory.query();
             console.log($scope.leaders);
 
